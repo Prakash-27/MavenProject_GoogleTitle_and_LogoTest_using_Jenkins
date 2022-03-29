211dc5fc9e6a7e3191815489c2f7a8ba653287e9
Prakash-27 <prakashs23007@gmail.com>
2022-03-29 01:44:54 -0500

    Parameters passing of Browser with Xml using <systemPropertyVariables> <browser>${Browser}</browser> </systemPropertyVariables> and in Test Suite

--- src/test/java/com/Google/GoogleTitleAndLogoTest.java
+++ src/test/java/com/Google/GoogleTitleAndLogoTest.java
@@ -4,23 +4,36 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleTitleAndLogoTest {
 	
-WebDriver driver;
+    WebDriver driver;
 	
+    @Parameters("Browser")
 	@BeforeMethod
-	public void setUp() {
+	public void setUp(String browserName) {
+		System.out.println("Parameter value is " + browserName);
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Prakash\\eclipse-workspace\\JenkinsGooglePagelaunch\\chromedriver.exe"); 
-        driver = new ChromeDriver();
+		if(browserName.contains("Chrome")) {
+			//we can also use WebdriverManager to set up Chrome driver -> WebDriverManager.chromedriver().setup();
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Prakash\\eclipse-workspace\\JenkinsGooglePagelaunch\\chromedriver.exe");
+			driver = new ChromeDriver();
+			
+		}else if(browserName.contains("Firefox")) {
+			//we can also use WebdriverManager to set up Firefox driver -> WebDriverManager.Firefoxdriver().setup();
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\Prakash\\eclipse-workspace\\JenkinsGooglePagelaunch\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("http://www.google.com");
 	}
